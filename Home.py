0ca2aa79470daddddf2c843315d6975bba07c594
Pedro Franca <pedrohenrique.franca@external.technipfmc.com>
2022-12-27 16:40:20 -0300

    commit 3

--- Home.py
+++ Home.py
@@ -3,7 +3,6 @@ import eap_roadmap
 import eap_compare
 import pandas as pd
 import base64
-import io
 import components.login as authenticator
 import openpyxl
 
@@ -28,19 +27,22 @@ if st.session_state["authenticated"] == True:
    
   
    uploaded_file = st.file_uploader('Choose a Mero 2 EAP file', type='xlsx')
+ 
    uploaded_file2 = st.file_uploader('Choose a Mero 2 EAP file from last month', type='xlsx')
       
  
    
-   if uploaded_file is not None:
+   if uploaded_file is not None:     
        #st.header("Sum Results")
        # Input Excel
        st.session_state.resultados = eap_roadmap.roadmap(uploaded_file) 
+              
        
-       # Cria Colunas
        df = pd.DataFrame(st.session_state.resultados.items(), columns=['LEVEL', 'SUM']) 
        df[['SUM', 'STATUS']] = df['SUM'].str.split(", ", 1, expand=True)
        df[['LEVEL', 'DESCRIPTION']] = df['LEVEL'].str.split(' -- ', 1, expand=True)
+       
+       
        
        option = st.sidebar.selectbox(
               "Filter table",
@@ -66,52 +68,61 @@ if st.session_state["authenticated"] == True:
       
        
        finalDate = optionMonth + optionYear           
-           
-             
-           
-       if option == " ":       
-           st.dataframe(df.iloc[:,[0,3,1,2]].style.applymap(eap_roadmap.color_background, subset=['STATUS']))
-       elif option == "INCORRECT SUM":
-           df.query("STATUS == 'INCORRECT SUM'", inplace = True)
-           st.dataframe(df.iloc[:,[0,3,1,2]].style.applymap(eap_roadmap.color_background, subset=['STATUS']))  
-       else: 
-           df.query("STATUS == 'CORRECT SUM'", inplace = True)
-           st.dataframe(df.iloc[:,[0,3,1,2]].style.applymap(eap_roadmap.color_background, subset=['STATUS'])) 
-           
+              
    
        if uploaded_file2 is not None:
            print("Upload realizado arquivo 2")
            
-           #Compare 2 files:
            check = st.sidebar.checkbox("Date selected", key="disabled")
            
-           if check:                                      
+           if check:                                                 
               #st.sidebar.markdown("Click [here](uploaded_file) to download the file.")
               #st.sidebar.download_button(label="Click here to download the file.", data=compareFiles, file_name='myresults.xlsx', mime='text/xlsx') 
               
-              st.markdown("Click the button below to download the manipulated file")
-              wb = openpyxl.load_workbook(uploaded_file)
+              st.markdown("Click the button below to download the manipulated file")              
            
-              if st.button("Download"): 
-                  ws = wb['EAP Mero 2']  
-                  wb.remove(wb['EAP Mero 2'])
-                  wb.create_sheet('EAP Mero 2', 0)
-                  new_ws = wb['EAP Mero 2']  
-                  ws = eap_compare.write_to_excel(uploaded_file, uploaded_file2, finalDate)
+              if st.button("Download"):                
+
+                  ws = eap_compare.write_to_excel(uploaded_file, uploaded_file2, finalDate) 
+                  my_yellow = openpyxl.styles.colors.Color(rgb='FFFF00')
+                  my_fill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=my_yellow)            
+                  
+                  #workbook = openpyxl.load_workbook(uploaded_file, data_only=True)
+                  workbook = openpyxl.load_workbook(uploaded_file, data_only=False)
+                  ws2 = workbook['EAP Mero 2'] 
+                  workbook.remove(ws2)
+                  new_ws = workbook.create_sheet('EAP Mero 2', 0)
+                  
                   for row in ws.rows:
-                     for cell in row:
-                         new_ws[cell.coordinate] = cell.value  
-                
-                  wb.save(filename='manipulated.xlsx')
-                  print("Binário criado")
-                  with open('manipulated.xlsx', 'rb') as f:
+                    for cell in row:
+                        new_cell = new_ws.cell(row=cell.row, column=cell.column, value=cell.value)  # Copy cell value
+                        new_cell.font = cell.font.copy()  # Copy font formatting
+                        new_cell.border = cell.border.copy()  # Copy border formatting
+                        new_cell.fill = cell.fill.copy()  # Copy fill formatting
+                        new_cell.number_format = cell.number_format  # Copy number formatting
+                        new_cell.protection = cell.protection.copy()  # Copy protection formatting
+                        new_cell.alignment = cell.alignment.copy()  # Copy alignment formatting
+                        
+                  fileName = uploaded_file.name
+                  workbook.save(filename=fileName)
+                  with open(fileName, 'rb') as f:
                       excel_file = f.read()  
-                  buf = io.BytesIO(excel_file)
-                  excel_file_b64 = base64.b64encode(excel_file).decode('utf-8')
-                  st.markdown(f'<a href="data:application/octet-stream;base64,{excel_file_b64}" download="manipulated.xlsx">Download manipulated file</a>', unsafe_allow_html=True)                                                     
+                  excel_file_b64 = base64.b64encode(excel_file).decode('utf-8')                                            
+                  st.markdown(f'<a href="data:application/octet-stream;base64,{excel_file_b64}" download={fileName}>Download manipulated file</a>', unsafe_allow_html=True)    
+
+                    
+       else:
+            if option == " ":       
+                st.dataframe(df.iloc[:,[0,3,1,2]].style.applymap(eap_roadmap.color_background, subset=['STATUS']))
+            elif option == "INCORRECT SUM":
+                df.query("STATUS == 'INCORRECT SUM'", inplace = True)
+                st.dataframe(df.iloc[:,[0,3,1,2]].style.applymap(eap_roadmap.color_background, subset=['STATUS']))  
+            else: 
+                df.query("STATUS == 'CORRECT SUM'", inplace = True)
+                st.dataframe(df.iloc[:,[0,3,1,2]].style.applymap(eap_roadmap.color_background, subset=['STATUS']))
        
    else:
-       st.warning('you need to upload a excel file.')
+       st.warning('you need to upload an excel file.')
        
        optionMonth = st.sidebar.selectbox(
               "Choose EAP Month",
@@ -128,11 +139,11 @@ if st.session_state["authenticated"] == True:
           )
        
        add_selectbox = st.sidebar.selectbox(
-           "Filter table",
-           (" ","CORRECT SUM", "INCORRECT SUM"),
-           disabled = True,
-           label_visibility = "hidden"
-       )
+            "Filter table",
+            (" ","CORRECT SUM", "INCORRECT SUM"),
+            disabled = True,
+            label_visibility = "hidden"
+        )
        
    authenticator.button_logout()
 
--- eap_compare.py
+++ eap_compare.py
@@ -2,35 +2,33 @@ import openpyxl
 import datetime
 
 
-
-
 def write_to_excel(file1, file2, date):
-    #Abrir arquivo Excel
-    workbook = openpyxl.load_workbook(file1, data_only=True)
-    workbook2 = openpyxl.load_workbook(file2, data_only=True)
+    # Open Excel file
+    workbook = openpyxl.load_workbook(file1)
+    workbook2 = openpyxl.load_workbook(file2)
 
-    # Seleciona a guia
+    # Select worksheet
     ws = workbook.worksheets[0]
     ws2 = workbook2.worksheets[0]
 
-    # Cor célula
+    # Get cell style
     my_yellow = openpyxl.styles.colors.Color(rgb='FFFF00')
     my_fill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=my_yellow)
+    my_style = openpyxl.styles.NamedStyle(name='highlight', fill=my_fill)
 
-    # Obter data do primeiro dia do mês atual
+    # Get date of first day of current month
     finalDate = datetime.datetime.strptime(date, "%b%Y").strftime("%Y-%m-%d")
 
-    # Index da coluna
+    # Get column index
     for row in ws.iter_rows():
         for cell in row:          
             if str(finalDate) in str(cell.value):
-               index = cell.column - 2
-               indexMonth = cell.column - 1
+                index = cell.column - 2
+                indexMonth = cell.column - 1
             elif cell.value == 'Ref.':
-               indexRef = cell.column - 1
+                indexRef = cell.column - 1
 
-
-    # Criar dicionário com Excel do mês anterior
+    # Create dictionary with data from previous month's Excel file
     num = 1
     key = 'Forecast ' + str(num)
     forecast = {key: []}
@@ -47,42 +45,26 @@ def write_to_excel(file1, file2, date):
 
     size = len(forecast)
 
-    # Substituir valores das keys por números
+    # Replace keys with numbers in dictionary
     numForecast = {}
-
     for i, key in enumerate(forecast):
-      # add the key and value to numbered_dict
-      numForecast[i] = forecast[key] 
+        numForecast[i] = forecast[key] 
                    
-    # Loop pintar célula   
+    # Loop to highlight cells
     num2 = 0
     num3 = 2
     for row in ws.iter_rows():
-        if row[indexRef].value == 'Baseline 1' and row[index].value != row[indexMonth].value: #verificar se é Baseline ou Actual
-           row[indexMonth].fill = my_fill
+        if row[indexRef].value == 'Baseline 1' and row[index].value != row[indexMonth].value: # Check if Baseline or Actual
+            row[indexMonth].style = my_style
         elif row[indexRef].value == 'Forecast':
-           forecastList = numForecast[num2]                      
-           for cell in row:               
-               if num3 <= len(forecastList) - 1 and cell.column > indexMonth and cell.value != None and cell.value != 0 and cell.value != forecastList[num3]:                               
-                   cell.fill = my_fill   
-                   num3 += 1
+            forecastList = numForecast[num2]                      
+            for cell in row:               
+                if num3 <= len(forecastList) - 1 and cell.column > indexMonth and cell.value != None and cell.value != 0 and cell.value != forecastList[num3]:                               
+                    cell.style = my_style   
+                    num3 += 1
         num2 += 1
         num3 = 2
         if num2 > size:
             break
-
-    # Salvar o arquivo Excel
-    #workbook.save(file1)
-    return ws
-    #return ws
-    print("COMPARAÇÃO REALIZADA")
     
-
-# file1 = 'Exemplos\\file1.xlsx'
-# file2 = 'Exemplos\\file2.xlsx'
-# date = 'Oct2022'
-
-# write_to_excel(file1, file2, date)
-
-
-  
+    return ws
